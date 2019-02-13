261a74532aa1f4d8aae1db4c7ed51e1749467e8c
raffyalfred <35229077+raffyalfred@users.noreply.github.com>
2019-02-13 10:51:45 -0700

    custom header
    added custom header wordpress loop

--- functions.php
+++ functions.php
@@ -68,6 +68,15 @@ if ( ! function_exists( 'zion_church_setup' ) ) :
 
 		// Add theme support for selective refresh for widgets.
 		add_theme_support( 'customize-selective-refresh-widgets' );
+		
+		$args = array(
+			'flex-width'    => true,
+			'width'         => 980,
+			'flex-height'    => true,
+			'height'        => 200,
+			'default-image' => get_template_directory_uri() . '/images/header.jpg',
+		);
+		add_theme_support( 'custom-header', $args );
 
 		/**
 		 * Add support for core custom logo.
