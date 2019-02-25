76a26f4352a33345ec2c05b60552f8378af487c6
raffyalfred <35229077+raffyalfred@users.noreply.github.com>
2019-02-25 10:27:10 -0700

    navigation
    menu and menu items flex direction column for mobile. styled menu button

--- header.php
+++ header.php
@@ -47,12 +47,13 @@
 		<div class="custom-header-image">
 		<img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />
 		</div>
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><i class="fas fa-bars"></i><?php esc_html_e( '', 'zion_church' ); ?></button>
+		<nav id="site-navigation" class="navigation main-navigation">
+			<button class="menu-toggle navigation__button" aria-controls="primary-menu" aria-expanded="false">MENU<?php esc_html_e( '', 'zion_church' ); ?></button>
 			<?php
 			wp_nav_menu( array(
 				'theme_location' => 'menu-1',
 				'menu_id'        => 'primary-menu',
+				'menu_class'	 => 'navigation__items',
 			) );
 			?>
 		</nav><!-- #site-navigation -->
