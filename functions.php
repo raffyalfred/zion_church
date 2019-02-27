02f0670839d8c68d6b88c6a97203e534184531de
Ray Garvez <ralfredgarvez@gmail.com>
2019-02-26 20:46:49 -0700

    header/nav
    custom logo and nav

--- functions.php
+++ functions.php
@@ -35,6 +35,7 @@ if ( ! function_exists( 'zion_church_setup' ) ) :
 		 */
 		add_theme_support( 'title-tag' );
 		add_theme_support( 'custom-header' );
+		add_theme_support( 'custom-logo' );
 
 		/*
 		 * Enable support for Post Thumbnails on posts and pages.
--- header.php
+++ header.php
@@ -28,6 +28,9 @@
 		<div class="site-branding header__hero">
 			<?php
 			the_custom_logo();
+			
+			
+
 			if ( is_front_page() && is_home() ) :
 				?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
@@ -42,25 +45,32 @@
 				?>
 				<p class="site-description"><?php echo $zion_church_description; /* WPCS: xss ok. */ ?></p>
 			<?php endif; ?>
-		</div><!-- .site-branding -->
-		
-		<div class="custom-header">
-			<div class="custom-header__image">
-			<img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />
-			</div>
+			
 			<div id="site-navigation" class="navigation main-navigation">
 				<nav>
 					<button class="menu-toggle navigation__button" aria-controls="primary-menu" aria-expanded="false"><i class="fas fa-bars"></i><?php esc_html_e( '', 'zion_church' ); ?></button>
 					<?php
-					wp_nav_menu( array(
-						'theme_location' => 'menu-1',
-						'menu_id'        => 'primary-menu',
-						'menu_class'	 => 'navigation__items',
-					) );
+						wp_nav_menu( array(
+							'theme_location' => 'menu-1',
+							'menu_id'        => 'primary-menu',
+							'menu_class'	 => 'navigation__items',
+						) );
 					?>
 				</nav><!-- #site-navigation -->
 			</div>
+		</div><!-- .site-branding -->
+		<div class="custom-header">
+				<div class="custom-header__image">
+					<img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />
+				</div>
 		</div>
+		
+
+		<!-- <div class="custom-header">
+			<div class="custom-header__image">
+			<img src="<?php // header_image(); ?>" height="<?php // echo get_custom_header()->height; ?>" width="<?php // echo get_custom_header()->width; ?>" alt="" />
+			</div>
+		</div> -->
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
